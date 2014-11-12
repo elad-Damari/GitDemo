3331550c5014fb57818ece75a98996010685fe24
Nadav <nadav.kershner@gmail.com>
2014-11-12 11:17:31 +0200

    Initial Commit

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by Nadav Kershner on 11/12/14.
+//  Copyright (c) 2014 Nadav Kershner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by Nadav Kershner on 11/12/14.
+//  Copyright (c) 2014 Nadav Kershner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
